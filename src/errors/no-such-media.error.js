ba00c6daa9f15317ba4e575a3134d625f8539879
serjio1905 <serjio.rv@gmail.com>
2018-11-03 15:55:01 +0200

    20181103_3

--- src/errors/no-such-media.error.js
+++ src/errors/no-such-media.error.js
@@ -1,6 +1,6 @@
 module.exports = class NoSuchMediaError extends Error {
   constructor(message, status) {
-    super(message);
+    super(`${message} with given parama was not found.`);
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
     this.status = status || 500;
