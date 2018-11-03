186cc5b42833a975b33d8d1d45f4f4022d271d4d
serjio1905 <serjio.rv@gmail.com>
2018-11-03 19:43:03 +0200

    20181103_4

--- __tests__/services/permision-service.test.js
+++ __tests__/services/permision-service.test.js
@@ -1,3 +1,5 @@
+// process.env.NODE_ENV=test;
+// const config = require('config');
 const PermissionService = require('../../src/services/permissions.service');
 const permissionModel = require('../../src/dto/permission.item').model;
 const request = require('supertest');
@@ -9,7 +11,7 @@ describe('/api/permission', () => {
         server = require('../../src/index.js');
     });
     afterEach(async () => {
-        await permissionModel.remove({});
+        // await permissionModel.remove({});
         server.close();
     });
     describe('POST /', () => {
@@ -17,7 +19,7 @@ describe('/api/permission', () => {
         const exec = async () => {
             return await request(server).post('/api/permissions').send(item);
         };
-        it('should return 200 and save permission item', async () => {
+        it('should return status 200', async () => {
             item = {
                 resource: "test resourse",
                 method: "test method",
@@ -25,6 +27,15 @@ describe('/api/permission', () => {
             }
             const result = await exec();
             expect(result.status).toBe(200);
+            const record = permissionModel.find({resource: "test resourse" });
+            expect(record).not.toBeNull();
+        });
+        it('should save permission item', async () => {
+            item = {
+                resource: "test resourse",
+                method: "test method",
+                description: "test description"
+            }
             const record = permissionModel.find({resource: "test resourse" });
             expect(record).not.toBeNull();
         });
